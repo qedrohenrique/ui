e71f8f4174bc0b38c0c3a5424d211aed0bc345ae
Pedro Henrique <pedro.005henr@gmail.com>
2025-10-31 21:16:51 -0300

    feat: navbar update

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,21 +1,28 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "./ui/button";
-import { GithubIcon } from "lucide-react";
+import { GithubIcon, HomeIcon } from "lucide-react";
 import Link from "next/link";
 
 export function Navbar() {
   return (
-    <nav className="flex items-center justify-end bg-primary/2 p-4 border-b-1 gap-2">
-      <Link
-        href="https://github.com/qedrohenrique/ui"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Button variant="ghost" size="icon">
-          <GithubIcon />
-        </Button>
-      </Link>
-      <ModeToggle />
+    <nav className="flex items-center justify-between bg-primary/2 p-4 border-b-1 gap-2">
+      <div className="flex items-center gap-2">
+        <Link href="/">
+          <Button variant="ghost" size="icon">
+            <HomeIcon />
+          </Button>
+        </Link>
+        <Link
+          href="https://github.com/qedrohenrique/ui"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button variant="ghost" size="icon">
+            <GithubIcon />
+          </Button>
+        </Link>
+        <ModeToggle />
+      </div>
     </nav>
   );
 }
